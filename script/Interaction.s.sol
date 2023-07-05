70f765b956c8f0119f06ad61bc479931e5743972
garv-aga <garvagarwal2003@gmail.com>
2023-07-05 13:34:38 +0530

    initial

---
+++ script/Interaction.s.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundFundMe is Script {
+    uint constant SEND_VALUE = 0.01 ether;
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+        fundFundMe(mostRecentlyDeployed);
+    }
+
+    function fundFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
+        vm.stopBroadcast();
+        console.log("Funded with %s", SEND_VALUE);
+    }
+}
+
+contract WithdrawFundMe is Script {
+    function withdrawFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).withdraw();
+        vm.stopBroadcast();
+        console.log("Withdraw FundMe balance!");
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+        withdrawFundMe(mostRecentlyDeployed);
+    }
+}
